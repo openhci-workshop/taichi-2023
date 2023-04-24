1cbb9b3387fea0a890144a059a3abd9a43135f94
Matthew <matthew6303@gmail.com>
2023-04-24 23:57:49 +0800

    feat: refactor project structure

---
+++ app/components/organisms/Header/index.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const Header = () => {
+	return (
+		<div className="flex items-center justify-between p-8 fixed top-0 left-0 right-0 border-b-2 h-16">
+			<Link href="/">
+				<h1 className="text-4xl">TAICHI LOGO</h1>
+			</Link>
+			<nav>
+				<Link href="/submission">Submission</Link>
+			</nav>
+		</div>
+	);
+};
+
+export default Header;
