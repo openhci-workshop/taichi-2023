a948b1e3f7999e29f7a36e15bcb91f0642bb9b8b
XmanLCH <xmanlch@gmail.com>
2023-05-22 21:22:12 +0800

    add metadata in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,23 @@
+import Head from 'next/head';
+
 const HomePage = () => {
-	return <div />;
+	return (
+		<div> 
+		    <Head>
+        		<title>TAICHI 2023</title>
+        		<meta name="description" content="TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。" key="desc" />
+        		<meta property="og:title" content="TAICHI 2023" />
+        		<meta
+          			property="og:description"
+          			content="TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。"
+        		/>
+        		<meta
+          			property="og:image"
+          			content="https://taichi2023.taiwanchi.org/og.png"
+        		/>
+      		</Head>
+		</div>
+	);
 };
 
 export default HomePage;
