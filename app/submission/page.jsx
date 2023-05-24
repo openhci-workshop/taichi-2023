89fda8df9fa8361c32801a6c0ea5fc12060b0638
Derek Lu <derekdylu@gmail.com>
2023-05-25 02:56:08 +0800

    fix seo

--- app/submission/page.jsx
+++ app/submission/page.jsx
@@ -12,8 +12,7 @@ export const metadata = {
 	keywords: '',
 	openGraph: {
 		title: 'TAICHI 2023',
-		description: 'TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，\
-		旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。',
+		description: 'TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。',
 		url: 'https://taichi2023.taiwanchi.org',
 		siteName: 'TAICHI',
 		images: [
