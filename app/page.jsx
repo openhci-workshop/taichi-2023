3919e64cd68eb4a2bc5f3f8055d650097f181126
Derek Lu <derekdylu@gmail.com>
2023-05-25 02:39:54 +0800

    feat: try seo

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,29 @@
 import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 
 const HomePage = () => {
 	return (
 		<div> 
-		    <Head>
-        		<title>TAICHI 2023</title>
-        		<meta name="description" content="TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。" key="desc" />
-        		<meta property="og:title" content="TAICHI 2023" />
-        		<meta
-          			property="og:description"
-          			content="TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。"
-        		/>
-        		<meta
-          			property="og:image"
-          			content="https://taichi2023.taiwanchi.org/og.png"
-        		/>
-      		</Head>
+			<NextSeo
+				title='TAICHI 2023'
+				description='TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。'
+				canonical='https://taichi2023.taiwanchi.org/'
+				openGraph={{
+					url: 'https://taichi2023.taiwanchi.org/',
+					title: 'TAICHI 2023',
+					description: 'TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。',
+					images: [
+						{
+							url: 'https://taichi2023.taiwanchi.org/og.png',
+							width: 800,
+							height: 600,
+							alt: 'TAICHI 2023',
+							type: 'image/png',
+						}
+					],
+					siteName: 'TAICHI 2023',
+				}}
+			/>
 		</div>
 	);
 };
