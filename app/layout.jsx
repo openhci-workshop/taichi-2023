afe8c61bf5a10df49878689e4863aa62f4b38873
Derek Lu <derekdylu@gmail.com>
2023-06-24 00:17:09 +0800

    change site name

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,7 +17,7 @@ export const metadata = {
 		description:
 			'TAICHI 2023（第九屆臺灣人機互動研討會）為臺灣人機互動學會主辦的年度研討會，旨在集結在臺灣進行科技、設計、人因等多元文化、背景、個人特質的研究人員與從業者交流互動。',
 		url: process.env.NEXT_PUBLIC_BASE_FETCH_URL,
-		siteName: 'TAICHI',
+		siteName: 'TAICHI 2023',
 		images: [
 			{
 				url: `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/og.png`,
