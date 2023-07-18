5f4375752a97aeb6999975fe6b9132ed771ba5ee
Derek Lu <derekdylu@gmail.com>
2023-07-18 16:41:39 +0800

    add revalidating

--- app/page.jsx
+++ app/page.jsx
@@ -35,7 +35,11 @@ const aldrich = Aldrich({
 });
 
 async function fetchContent() {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/api/submission`)
+	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/api/submission`,{
+		next: {
+			revalidate: 60,
+		},
+	})
  
   return res.json()
 }
