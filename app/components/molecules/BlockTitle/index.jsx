2b862177bebb1a3884ec5d6eba8a44a125aa4130
Derek Lu <derekdylu@gmail.com>
2023-07-18 20:26:08 +0800

    fix: auto scroll problem

--- app/components/molecules/BlockTitle/index.jsx
+++ app/components/molecules/BlockTitle/index.jsx
@@ -1,10 +1,5 @@
-'use client';
-
-import { useLayoutEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
 import { Noto_Sans_TC, Aldrich } from 'next/font/google';
-import { gsap } from 'gsap/dist/gsap';
-import { useTrail, a } from '@react-spring/web';
 
 const notoSansTC = Noto_Sans_TC({
 	weight: ['400', '500', '700'],
@@ -18,59 +13,14 @@ const aldrich = Aldrich({
 
 import styles from './styles.module.scss';
 
-const BlockTitle = ({ titleZh, titleEn }) => {
-	const [start, setStart] = useState(false);
-	const titleRef = useRef(null);
-	const titleZhItems = titleZh.split('');
-	const titleEnItems = titleEn.split('');
-
-	const config = {
-		config: { mass: 5, tension: 10000, friction: 400 },
-		// opacity: start ? 1 : 0,
-		y: start ? '0%' : '100%',
-		reverse: !start,
-	};
-
-	const trail = useTrail(titleZhItems.length + titleEnItems.length, config);
-
-	useLayoutEffect(() => {
-		gsap.to(titleRef.current, {
-			scrollTrigger: {
-				trigger: titleRef.current,
-				start: 'top 80%',
-				end: 'bottom 80%',
-				scrub: 1,
-				onEnter: () => setStart(true),
-				onLeaveBack: () => setStart(false),
-			},
-		});
-	});
-
-	return (
-		<div className={classnames('flex flex-col', styles.blockTitle)} ref={titleRef}>
-			<h2 className={classnames('flex overflow-hidden', notoSansTC.className)}>
-				{trail.map((style, index) =>
-					index < titleZhItems.length ? (
-						<a.p key={index} style={style}>
-							{titleZhItems[index]}
-						</a.p>
-					) : (
-						<a.span
-							key={index}
-							className={classnames(aldrich.className, {
-								'ml-2 md:ml-3': index === titleZhItems.length,
-								'min-w-[8px] md:min-w-[24px]': titleEnItems[index] === ' ',
-							})}
-							style={style}
-						>
-							{titleEnItems[index - titleZhItems.length]}
-						</a.span>
-					)
-				)}
-			</h2>
-			<div />
-		</div>
-	);
-};
+const BlockTitle = ({ titleZh, titleEn }) => (
+	<div className={classnames('flex flex-col', styles.blockTitle)}>
+		<h2 className={notoSansTC.className}>
+			{titleZh}
+			<span className={aldrich.className}>{titleEn}</span>
+		</h2>
+		<div />
+	</div>
+);
 
 export default BlockTitle;
--- app/components/molecules/SectionTitle/index.jsx
+++ app/components/molecules/SectionTitle/index.jsx
@@ -1,10 +1,5 @@
-'use client';
-
-import { useLayoutEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
 import { Noto_Sans_TC, Aldrich } from 'next/font/google';
-import { gsap } from 'gsap/dist/gsap';
-import { useTrail, a } from '@react-spring/web';
 
 const notoSansTC = Noto_Sans_TC({
 	weight: ['400', '500', '700'],
@@ -16,69 +11,25 @@ const aldrich = Aldrich({
 	subsets: ['latin'],
 });
 
-const SectionTitle = ({ className, titleZh, titleEn }) => {
-	const [start, setStart] = useState(false);
-	const titleRef = useRef(null);
-	const titleZhItems = titleZh.split('');
-	const titleEnItems = titleEn.split('');
-
-	const config = {
-		config: { mass: 5, tension: 10000, friction: 400 },
-		// opacity: start ? 1 : 0,
-		y: start ? '0%' : '100%',
-		reverse: !start,
-	};
-
-	const trailZh = useTrail(titleZhItems.length, config);
-	const trailEn = useTrail(titleEnItems.length, config);
-
-	useLayoutEffect(() => {
-		gsap.to(titleRef.current, {
-			scrollTrigger: {
-				trigger: titleRef.current,
-				start: 'top 80%',
-				end: 'bottom 80%',
-				scrub: 1,
-				onEnter: () => setStart(true),
-				onLeaveBack: () => setStart(false),
-			},
-		});
-	});
-
-	return (
-		<div className={classnames('flex flex-col', className)} ref={titleRef}>
-			<h1
-				className={classnames(
-					notoSansTC.className,
-					'flex text-white font-semibold text-xl md:text-4xl lg:text-5xl leading-tight mb-4 overflow-hidden z-[8]'
-				)}
-			>
-				{trailZh.map((style, index) => (
-					<a.p key={index} style={style}>
-						{titleZhItems[index]}
-					</a.p>
-				))}
-			</h1>
-			<h1
-				className={classnames(
-					aldrich.className,
-					'flex uppercase text-white font-normal leading-tight text-lg md:text-4xl lg:text-5xl mb-8 md:mb-16 overflow-hidden z-[8]'
-				)}
-			>
-				{trailEn.map((style, index) => (
-					<a.p
-						key={index}
-						className={classnames({
-							'min-w-[8px] md:min-w-[24px]': titleEnItems[index] === ' ',
-						})}
-						style={style}
-					>
-						{titleEnItems[index]}
-					</a.p>
-				))}
-			</h1>
-		</div>
-	);
-};
+const SectionTitle = ({ className, titleZh, titleEn }) => (
+	<div className={classnames('flex flex-col', className)}>
+		<h1
+			className={classnames(
+				notoSansTC.className,
+				'text-white font-semibold text-xl md:text-4xl lg:text-5xl leading-tight mb-4'
+			)}
+		>
+			{titleZh}
+		</h1>
+		<h1
+			className={classnames(
+				aldrich.className,
+				'uppercase text-white font-normal leading-tight text-lg md:text-4xl lg:text-5xl mb-8 md:mb-16'
+			)}
+		>
+			{titleEn}
+		</h1>
+	</div>
+);
 
 export default SectionTitle;
