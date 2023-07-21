450599246ef70d5af49fa36bedfb51e1d2459f7a
Derek Lu <derekdylu@gmail.com>
2023-07-22 02:43:05 +0800

    fix: navbar y scroll

--- app/components/organisms/NavBar/index.jsx
+++ app/components/organisms/NavBar/index.jsx
@@ -62,7 +62,8 @@ const NavBar = () => {
 
   function scrollToElement(id) {
     const element = document.getElementById(id); // Replace 'targetElement' with the ID of the element you want to scroll to
-    const offsetTop = element.offsetTop + 350;
+    const motionOffset = width > MIN_WIDTH ? 350 : 300;
+    const offsetTop = element.offsetTop + motionOffset;
     window.scrollTo({
       top: offsetTop,
       behavior: 'smooth' // Add smooth scrolling behavior
